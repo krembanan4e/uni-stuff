90192fb49e54bc99f9a5848c2e3d15a08bef94fb
krembanan4e <148962937+krembanan4e@users.noreply.github.com>
2023-10-30 18:01:20 +0100

    Update and rename aud 4-9 (samoglaska ili soglaska SWITCH).cpp to aud 4-9 (samoglaska, cifra ili drugo so SWITCH).cpp

--- SP/auditoriski/aud 4-9 (samoglaska, cifra ili drugo so SWITCH).cpp
+++ SP/auditoriski/aud 4-9 (samoglaska, cifra ili drugo so SWITCH).cpp
@@ -19,8 +19,20 @@ int main() {
         case 'u':
             cout << "Samoglaska.";
             break;
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            cout << "Cifra.";
+            break;
         default:
-            cout << "Soglaska.";
+            cout << "Drugo.";
 
     }
 
