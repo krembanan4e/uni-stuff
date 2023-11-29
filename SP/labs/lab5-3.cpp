2ebdde5aaf7156c05770987b4dd684e2e3f04b2c
krembanan4e <148962937+krembanan4e@users.noreply.github.com>
2023-11-29 09:34:10 +0100

    Create lab5-3.cpp

---
+++ SP/labs/lab5-3.cpp
@@ -0,0 +1,23 @@
+/*Write a program that will read n pairs od natural numbers (n is first, than the n pairs)
+ * It is necessary to check for each read pair of numbers, whether all the digits od the first number
+ * appear in the second number. If this condition is met, the program will print YES, otherwise NO.
+ * Checking whether all the digits of one number appear in the second number should be done with the function appears(int first, int second)
+ * input                output
+ * 4                    NO
+ * 421988 4111992       NO
+ * 985 85643            NO
+ * 8546 1235            YES
+ * 3322 123  */
+
+#include <iostream>
+using namespace std;
+
+void appear (int first, int second){
+    
+}
+
+int main(){
+
+
+    return 0;
+}
