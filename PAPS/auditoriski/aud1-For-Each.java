52a469702171028e3e2f342d05713c787360845e
krembanan4e <148962937+krembanan4e@users.noreply.github.com>
2024-10-12 21:05:28 +0200

    Create aud1-For-Each.java

---
+++ PAPS/auditoriski/aud1-For-Each.java
@@ -0,0 +1,12 @@
+//struktura FOR-EACH
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] niza = {3, 9, 5, -5};
+
+        for (int broj: niza){
+            System.out.println(broj);
+        }
+    }
+}
